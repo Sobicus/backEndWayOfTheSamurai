26ba435ae996105940ad495102c8b1c79cf95bd5
Sobicus <83043123+Sobicus@users.noreply.github.com>
2023-08-16 21:30:38 +0200

    back End Way Of The Samurai

---
+++ m7/server.js
@@ -0,0 +1,71 @@
+const http = require('http');
+const fs = require('fs')
+
+const delayThruPromise = (ms) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve()
+        }, ms)
+    })
+}
+const readFilePromise = (path) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(path, (err, data) => {
+            if (err) reject(err)
+            else resolve(data)
+        })
+    })
+}
+
+const server = http.createServer(async (req, res) => {
+    switch (req.url) {
+        case '/about': {
+            //synchronous
+            const data = fs.readFileSync('pages/about.html')
+            res.write(data)
+            res.end()
+            break
+        }
+        case '/home': {
+            //asynchronous
+            fs.readFile('pages/home.html', (err, data) => {
+                if (err) res.write('500, page not found')
+                else res.write(data)
+                res.end()
+            })
+            break
+        }
+        case '/delay': {
+            setTimeout(() => {
+                res.write('delay 3 sec')
+                res.end()
+            }, 3000)
+            break
+        }
+        case '/delaywithpromise': {
+            delayThruPromise(3000)
+            res.write('delay 3 sec with promise')
+            res.end()
+            break
+        }
+        case '/main': {
+            const data = await readFilePromise('pages/main.html')
+            res.write(data)
+            res.end()
+            break
+        }
+        case '/': {
+            const data = 'My maine page'
+            res.write(data)
+            res.end()
+            break
+        }
+        default: {
+            res.write('404 not found')
+            res.end()
+
+        }
+    }
+})
+
+server.listen(3003)
